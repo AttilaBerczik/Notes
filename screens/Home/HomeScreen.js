d3f4c1cd82a22e6a54b4fd9486238c27c080c2e5
AttilaBerczik <50765811+AttilaBerczik@users.noreply.github.com>
2021-05-30 18:43:12 +0200

    Able to access and update the data offline
    To do this, I had eject the entire application

---
+++ screens/Home/HomeScreen.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import Notes from "./Notes";
+import CreateNoteButton from "./CreateNoteButton";
+import firestore from "@react-native-firebase/firestore";
+
+function HomeScreen({ route, navigation }) {
+    const [noteData, setNoteData] = useState([]);
+    const { userId } = route.params;
+    const docRef = firestore().doc("users/" + userId);
+    useEffect(() => {
+        const unsubscribe = docRef.onSnapshot((snapshot) => setNoteData(snapshot.data()));
+        return () => unsubscribe();
+    }, [docRef]);
+    return (
+        <View style={{ height: "100%" }}>
+            {noteData.n ? (
+                <>
+                    <Notes noteData={noteData.n} navigation={navigation} docRef={docRef} userId={userId} />
+                    <CreateNoteButton navigation={navigation} noteData={noteData.n} userId={userId} />
+                </>
+            ) : (
+                <></>
+            )}
+        </View>
+    );
+}
+
+export default HomeScreen;
---
+++ screens/Sign/SignUpScreen.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { TouchableOpacity, Button, TextInput, Text, View, StyleSheet } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import auth from "@react-native-firebase/auth";
+
+const SignupScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const signUp = () => {
+        auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                const storeData = async (value) => {
+                    try {
+                        await AsyncStorage.setItem("userId", value);
+                    } catch (e) {
+                        setError(e);
+                    }
+                };
+                //saving the user id in local storage
+                storeData(userCredential.user.uid);
+                navigation.navigate("Home", {
+                    userId: userCredential.user.uid,
+                });
+            })
+            .catch((error) => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                setError(errorCode, errorMessage);
+            });
+    };
+    return (
+        <View>
+            <Text>Email</Text>
+            <TextInput value={email} onChangeText={setEmail} keyboardType="email-address" style={styles.input} />
+            <Text>Password</Text>
+            <TextInput value={password} onChangeText={setPassword} secureTextEntry style={styles.input} />
+            {error ? <Text style={{ color: "red" }}>{error}</Text> : null}
+            <Button title="Sign Up" onPress={() => signUp()} />
+            <TouchableOpacity onPress={() => navigation.navigate("Signin")}>
+                <Text>Already have an account? Sign In</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        borderColor: "#575009",
+        alignSelf: "stretch",
+        margin: 25,
+        height: 50,
+        borderRadius: 6,
+        paddingHorizontal: 16,
+        fontSize: 20,
+        fontWeight: "300",
+    },
+});
+
+export default SignupScreen;
